f0aeb400f492ff102ce564bb0deb3a312179c42e
ego-prog <ego@ego.adv.br>
2021-12-15 16:33:57 -0300

    teste

---
+++ introducao-ao-javascript/teste/exemplo.js
@@ -0,0 +1,9 @@
+// cometário de linha | ctrl + ;
+/*
+Comentário de várias linhas
+*/
+
+// var preco = 2;
+var desconto = 0.2;
+const PRECO = 2; // constante é escrita em maiúscula
+var total = PRECO - desconto;
