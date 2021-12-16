cf032bf38a55c4f6095e8f4f9ffb7bdc5e4f3a4d
ego-prog <ego@ego.adv.br>
2021-12-15 21:19:28 -0300

    node

--- introducao-ao-javascript/teste/funcoes.js
+++ introducao-ao-javascript/teste/funcoes.js
@@ -1,7 +1,9 @@
-//função
-function soma(a, b)
+function returnEvenValues(array)
 {
-    console.log(a + b);
-    return a + b;
+    for(let i = 0; i <array.length; i++){
+        if (array[i % 2 == 0]){
+
+        }
+    }
+
 }
-soma(3, 5);
