69d93be8540f04f67b9c9a54e5ed1fafdb5acbe7
Emanoel Georgio de Oliveira <ego@ego.adv.br>
2021-12-19 19:55:41 -0300

    aula 3

---
+++ sintaxe-basica-em-javascript/aula 3/script.js
@@ -0,0 +1,6 @@
+// o que são vetores ou arrays
+/*let array =["string",1,false];
+console.log(array);*/
+//pode guardar varios tipos de dados
+let array = ['string', 1, true, ['array1.1', 'array1.2'], ['array2'], ['array3']];
+console.log(array[2]);
