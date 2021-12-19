25f3c3c7b71040a41320656a486c22fb0088a672
ego-prog <ego@ego.adv.br>
2021-12-18 23:24:08 -0300

    Aula 2

---
+++ sintaxe-basica-em-javascript/aula 2/script.js
@@ -0,0 +1,36 @@
+/* comentar em bloco
+shift + alt + a */
+// //tipos primitivos
+// //booleam
+// var vOuF = false;
+// console.log(typeof(vOuF));
+// //number
+// var numeroQualquer = 1;
+// console.log(typeof(numeroQualquer));
+// //string
+// var nome = "Emanoel";
+// console.log(typeof(nome));
+// // var escopo global
+// var variavel;
+// console.log(variavel);
+// // let scopo local
+// let variavel2 = "texto";
+// console.log(variavel2);
+// //constatante
+// const constatante = "valor";
+// console.log(constatante);
+// var escopoglobal = "global";
+// console.log(escopoglobal);
+// function escopoLocal(){
+//     let escopoLocalInterno = 'local';
+//     console.log(escopoLocalInterno);
+// }
+// escopoLocal();
+// "==" valores iguais
+/* var comparacao = '0' == 0;
+console.log(comparacao);
+// "===" valor e tipos iguais
+var comparacaoIdentica = '0' === 0;
+console.log(comparacaoIdentica); */
+var adicao = 1+1;
+var divisaoReal 
