f1e42d63cea08a8b2f8633e4898370cdfb79469a
Emanoel Georgio de Oliveira <ego@ego.adv.br>
2021-12-19 21:57:51 -0300

    Aula 3

--- sintaxe-basica-em-javascript/aula 3/script.js
+++ sintaxe-basica-em-javascript/aula 3/script.js
@@ -2,5 +2,16 @@
 /*let array =["string",1,false];
 console.log(array);*/
 //pode guardar varios tipos de dados
-let array = ['string', 1, true, ['array1.1', 'array1.2'], ['array2'], ['array3']];
-console.log(array[2]);
+let array = ['string', 1, true, ['array1.1'], ['array2'], ['array3']];
+// console.log(array[2]);
+// forEach
+//array.forEach(function(item, index){console.log(item, index)})
+array.push('novo Item')
+console.log(array);
+array.pop()
+console.log(array);
+array.shift()
+console.log(array);
+array.unshift("novo item")
+console.log(array);
+console.log(array.indexOf(true));
