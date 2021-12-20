43ffaad201b6a33330156c80dc9283a7f5694f9f
ego-prog <ego@ego.adv.br>
2021-12-20 17:55:41 -0300

    Correção aula 5

---
+++ sintaxe-basica-em-javascript/aula 5/script.js
@@ -0,0 +1,8 @@
+/* function funcao(){
+    console.log("Essa mensagem veio de uma função");
+}
+funcao(); */
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo)
+}
+mensagem("Tudo Certo", "Jovem");
