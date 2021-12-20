a46e320ef19a5b3d3da352ed1245470ff39d1c07
ego-prog <ego@ego.adv.br>
2021-12-20 17:56:41 -0300

    correção

--- sintaxe-basica-em-javascript/script.js
+++
@@ -1,8 +0,0 @@
-/* function funcao(){
-    console.log("Essa mensagem veio de uma função");
-}
-funcao(); */
-function mensagem(primeiro, segundo){
-    console.log(primeiro, segundo)
-}
-mensagem("Tudo Certo", "Jovem");
