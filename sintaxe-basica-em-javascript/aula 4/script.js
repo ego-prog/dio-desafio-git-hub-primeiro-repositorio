9708955f3e1c5d6eeaa509165e6a69e7f7771725
ego-prog <ego@ego.adv.br>
2021-12-20 14:49:14 -0300

    Aula 4

---
+++ sintaxe-basica-em-javascript/aula 4/script.js
@@ -0,0 +1,25 @@
+/*var jogador1 = 0;
+var jogador2 = 1;
+var placar;
+if (jogador1 != -1) {
+    if (jogador1 > 0 && jogador2 == 0) {
+        console.log("Jogador 1 marcou ponto");
+        placar = jogador1 > jogador2;
+    } else if (jogador2 > 0 && jogador1 == 0) {
+        console.log("Jogador 2 marcou ponto");
+        placar = jogador2 > jogador1;
+    } else {
+        console.log("Ninguém marcou ponto");
+    }
+}
+switch (placar) {
+    case placar = jogador1 > jogador2:
+        console.log("O Jogador 1 ganhor");
+        break;
+    case placar = jogador2 > jogador1:
+        console.log("O Jogador 2 ganhou");
+        break;
+    default:
+        console.log("Ninguém ganhou");
+
+}*/
