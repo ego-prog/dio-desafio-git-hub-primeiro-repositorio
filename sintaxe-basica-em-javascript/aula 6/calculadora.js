8562ad4e37698de2035a8b04a123dec727d15ba4
ego-prog <ego@ego.adv.br>
2021-12-20 17:44:14 -0300

    Aula 6

---
+++ sintaxe-basica-em-javascript/aula 6/calculadora.js
@@ -0,0 +1,4 @@
+function calculadora(){
+    const operacao = prompt ("Escolha uma operação\n1 - SOMA (+)\n2 - SUBTRAÇÃO (-)\n3 - MULTIPLICAÇÃO (*)\n4 - DIVISÃO REAL (/)\n5 - DIVISÃO INTEIRA (%)\n6 - POTENCIAÇÃO (**)")
+}
+calculadora();
---
+++ sintaxe-basica-em-javascript/aula 6/script.js
@@ -0,0 +1,17 @@
+// tipos de funções
+//declarativa
+/* function funcao(){
+    console.log('ok')
+}
+funcao(); */
+//expressão de função
+// com nomeação
+/* var funcao = function opcional(){
+    console.log("Sou uma mensagem de função de expressão")
+}
+funcao(); */
+// arrow function =>
+var funcao = () =>{
+    console.log("arrow")
+}
+funcao();
